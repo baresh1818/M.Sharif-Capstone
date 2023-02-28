fb2a079f0262fd08fd3fcdc645e2a86721f073ff
Mirwais hoshmand <mirwaishoshmand@Mirwaiss-Air.attlocal.net>
2023-02-28 14:34:24 -0800

    good to go

--- src/test/java/tek/sdet/framework/runner/TestRunner.java
+++ src/test/java/tek/sdet/framework/runner/TestRunner.java
@@ -9,7 +9,7 @@ import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
 @RunWith(Cucumber.class)
 @CucumberOptions
 (
-		tags ="@accountPageScenario1",                                   //test case name
+		tags ="@TCsignInSenario1",                                   //test case name
 		features = "classpath:features",
 		glue ="tek.sdet.framework",
 		dryRun = false,
